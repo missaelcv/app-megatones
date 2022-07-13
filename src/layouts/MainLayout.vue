bd3b93d7ac9ccf9870f285b08570aa248c42ef13
missaelcv <missaelcv0220@gmail.com>
2022-07-12 23:18:24 -0400

    prueba

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Megatones
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>Megatone v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
